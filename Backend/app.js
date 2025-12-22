377f990e4201676cd4b93e5d08c13176f1dab85d
Harun mansuri <mrharun961766@gmail.com>
2025-12-22 11:06:25 +0530

    Data base setup done

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express";
 const app = express();
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -12,6 +13,15 @@ app.use(urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Hello from the backend server!");
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
